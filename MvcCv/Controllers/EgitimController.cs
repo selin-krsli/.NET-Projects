283cbf2bb392ba286b08cd6c626d9c9b09f7d302
Selin Karslı <selin.krsli@gmail.com>
2024-09-10 15:12:01 +0300

    Adding Education Part of CV-Project

---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,63 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        GenericRepository<TBL_EDUCATION> repo = new GenericRepository<TBL_EDUCATION>();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TBL_EDUCATION model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(model);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var value = repo.Find(s=>s.ID == id);
+            repo.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimGüncelle(int id)
+        {
+            var value = repo.Find(s=>s.ID == id);
+            return View(value) ;
+        }
+        [HttpPost]
+        public ActionResult EgitimGüncelle(TBL_EDUCATION model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimGüncelle");
+            }
+            var value = repo.Find(s => s.ID == model.ID);
+            value.Baslik = model.Baslik;
+            value.AltBaslik1 = model.AltBaslik1;
+            value.AltBaslik2 = model.AltBaslik2;
+            value.GNO = model.GNO;
+            value.Tarih = model.Tarih;
+            repo.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MvcCv/Models/Entity/TBL_EDUCATION.cs
+++ MvcCv/Models/Entity/TBL_EDUCATION.cs
@@ -11,13 +11,16 @@ namespace MvcCv.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBL_EDUCATION
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="Bu alan boþ geçilemez.")]
         public string Baslik { get; set; }
         public string AltBaslik1 { get; set; }
         public string AltBaslik2 { get; set; }
+        [StringLength(10, ErrorMessage ="Lütfen en fazla 10 karakterlik veri giriþi yapýnýz.")]
         public string GNO { get; set; }
         public string Tarih { get; set; }
     }
