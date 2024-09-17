d1cfa15d8a1529cc12a3f2962a99913ab707b52a
Selin Karslı <selin.krsli@gmail.com>
2024-09-17 11:58:57 +0300

    Adding ContactController&View

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,30 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        GenericRepository<TBL_INTERESTS> repository = new GenericRepository<TBL_INTERESTS>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var values = repository.List();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(TBL_INTERESTS model)
+        {
+            TBL_INTERESTS entity = repository.Find(s=>s.ID == 1);
+            entity.Aciklama1 = model.Aciklama1;
+            entity.Aciklama2 = model.Aciklama2;
+            repository.TUpdate(entity);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCv/Controllers/IletisimController.cs
@@ -0,0 +1,20 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class IletisimController : Controller
+    {
+        GenericRepository<TBL_CONTACT> repository = new GenericRepository<TBL_CONTACT>();
+        public ActionResult Index()
+        {
+            var values = repository.List();
+            return View(values);
+        }
+    }
+}
