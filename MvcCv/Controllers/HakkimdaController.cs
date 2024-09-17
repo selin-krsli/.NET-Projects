adfb492cd7b59b6509ba5ba3bd4155663404bf05
Selin Karslı <selin.krsli@gmail.com>
2024-09-17 12:33:09 +0300

    Adding AboutController&View

--- MvcCv/Controllers/HakkimdaController.cs
+++ MvcCv/Controllers/HakkimdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,26 @@ namespace MvcCv.Controllers
 {
     public class HakkimdaController : Controller
     {
-        // GET: Hakkimda
+        GenericRepository<TBL_ABOUT> repository = new GenericRepository<TBL_ABOUT>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var values = repository.List();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(TBL_ABOUT model)
+        {
+            TBL_ABOUT entity = repository.Find(s => s.ID == 1);
+            entity.Ad = model.Ad;
+            entity.Soyad = model.Soyad;
+            entity.Adres = model.Adres;
+            entity.Telefon = model.Telefon;
+            entity.Mail = model.Mail;
+            entity.Aciklama = model.Aciklama;
+            entity.Resim = model.Resim;
+            repository.TUpdate(entity);
+            return RedirectToAction("Index");
         }
     }
 }
