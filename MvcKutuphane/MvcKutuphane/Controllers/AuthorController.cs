c8f1a898a6ffc58be98687f8588aa31bcfecc883
Selin Karslı <selin.krsli@gmail.com>
2024-09-22 00:20:04 +0300

    .

---
+++ MvcKutuphane/MvcKutuphane/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class AuthorController : Controller
+    {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var values = dbKutuphane.TBL_YAZAR.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateAuthor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateAuthor(TBL_YAZAR parametre)
+        {
+            dbKutuphane.TBL_YAZAR.Add(parametre);
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
