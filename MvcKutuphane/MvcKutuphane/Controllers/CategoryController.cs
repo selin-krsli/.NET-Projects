974fa37b5ec0ab8e1e555445498c1b23c91c9e18
Selin Karslı <selin.krsli@gmail.com>
2024-09-21 16:35:59 +0300

    Adding CategoryController and View

--- MvcKutuphane/MvcKutuphane/Controllers/CategoryController.cs
+++ MvcKutuphane/MvcKutuphane/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,44 @@ namespace MvcKutuphane.Controllers
 {
     public class CategoryController : Controller
     {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var values = dbKutuphane.TBL_KATEGORI.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(TBL_KATEGORI parametre)
+        {
+            dbKutuphane.TBL_KATEGORI.Add(parametre);
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = dbKutuphane.TBL_KATEGORI.Find(id);
+            dbKutuphane.TBL_KATEGORI.Remove(value);
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = dbKutuphane.TBL_KATEGORI.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(TBL_KATEGORI parametre)
+        {
+            var value = dbKutuphane.TBL_KATEGORI.Find(parametre.ID);
+            value.Ad = parametre.Ad;
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
