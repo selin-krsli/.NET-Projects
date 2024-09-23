f4deecf8d3ae3e9c282c85074677ea8d4352c652
Selin Karslı <selin.krsli@gmail.com>
2024-09-23 11:05:21 +0300

    Adding AuthorController&View

--- MvcKutuphane/MvcKutuphane/Controllers/AuthorController.cs
+++ MvcKutuphane/MvcKutuphane/Controllers/AuthorController.cs
@@ -27,5 +27,31 @@ namespace MvcKutuphane.Controllers
             dbKutuphane.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult DeleteAuthor(int id)
+        {
+            var value = dbKutuphane.TBL_YAZAR.Find(id);
+            dbKutuphane.TBL_YAZAR.Remove(value);
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateAuthor(int id)
+        {
+            var existingAuthor = dbKutuphane.TBL_YAZAR.Find(id);
+            return View(existingAuthor);
+        }
+        [HttpPost]
+        public ActionResult UpdateAuthor(TBL_YAZAR parametre)
+        {
+            var existingAuthor = dbKutuphane.TBL_YAZAR.Find(parametre.ID);
+            if(existingAuthor != null)
+            {
+                existingAuthor.Ad = parametre.Ad;
+                existingAuthor.Soyad = parametre.Soyad;
+                existingAuthor.Detay = parametre.Detay;
+                dbKutuphane.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
