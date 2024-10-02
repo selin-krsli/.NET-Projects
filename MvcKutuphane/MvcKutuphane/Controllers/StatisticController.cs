4b767c890997aacff7d1e58dac9d1eca10eee531
Selin Karslı <selin.krsli@gmail.com>
2024-10-02 14:47:30 +0300

    Adding new file and LinqExamples

--- MvcKutuphane/MvcKutuphane/Controllers/StatisticController.cs
+++ MvcKutuphane/MvcKutuphane/Controllers/StatisticController.cs
@@ -4,13 +4,23 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class StatisticController : Controller
     {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var value1 = dbKutuphane.TBL_UYELER.Count();
+            var value2 = dbKutuphane.TBL_KITAP.Count();
+            var value3 = dbKutuphane.TBL_KITAP.Where(s=>s.Durum==false).Count();
+            var value4 = dbKutuphane.TBL_CEZALAR.Sum(s => s.Para);
+            ViewBag.deger1=value1;
+            ViewBag.deger2=value2;
+            ViewBag.deger3=value3;  
+            ViewBag.deger4=value4;
             return View();
         }
         public ActionResult Weather()
@@ -34,6 +44,10 @@ namespace MvcKutuphane.Controllers
                 file.SaveAs(filePath);
             }
             return RedirectToAction("Galeri");
+        }
+        public ActionResult LinqCard()
+        {
+            return View();
         }
     }
 }
