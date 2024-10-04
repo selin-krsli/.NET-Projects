dbb11500b116aa8eb5658d0218e2515e77cf598b
Selin Karslı <selin.krsli@gmail.com>
2024-10-04 09:55:29 +0300

    Updated LoginController and View

--- MvcKutuphane/MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/MvcKutuphane/Controllers/LoginController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class LoginController : Controller
     {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
+        [HttpGet]
         public ActionResult EnterLogin()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult EnterLogin(TBL_UYELER parametre)
+        {
+            var LoginInfo = dbKutuphane.TBL_UYELER.FirstOrDefault(s=>s.Mail==parametre.Mail && s.Sifre==parametre.Sifre);
+            if(LoginInfo!=null)
+            {
+                FormsAuthentication.SetAuthCookie(LoginInfo.Mail, false);
+                Session["Mail"]=LoginInfo.Mail.ToString();
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- MvcKutuphane/MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/MvcKutuphane/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace MvcKutuphane.Controllers
 {
     public class PanelimController : Controller
     {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var kullaniciMail = (string)Session["Mail"];
+            var values = dbKutuphane.TBL_UYELER.FirstOrDefault(s=>s.Mail==kullaniciMail);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index2(TBL_UYELER parametre)
+        {
+            var kullanici = (string)Session["Mail"];
+            var member = dbKutuphane.TBL_UYELER.FirstOrDefault(s => s.Mail == kullanici);
+            member.Sifre = parametre.Sifre;
+            member.Ad = parametre.Ad;
+            member.Soyad = parametre.Soyad;
+            member.Fotograf = parametre.Fotograf;
+            member.Okul = parametre.Okul;
+            member.KullaniciAdi = parametre.KullaniciAdi;
+            dbKutuphane.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
