e7f2402c96e904bde230dae37b5119fb25d6c63f
Selin Karslı <selin.krsli@gmail.com>
2024-10-04 10:41:21 +0300

    Adding NewMessageController

---
+++ MvcKutuphane/MvcKutuphane/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class MessageController : Controller
+    {
+        DBKUTUPHANEEntities dbKutuphane = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var uyeMail = (string)Session["Mail"].ToString();
+            var messages = dbKutuphane.TBL_MESAJLAR.Where(s => s.Alici==uyeMail.ToString()).ToList();
+            return View(messages);
+        }
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+    }
+}
